ebfbacd0aa831b757416b4fe2f42b0f6f93b89b1
GailKH <83104213+GailKH@users.noreply.github.com>
2024-02-19 15:36:09 -0500

    Create gallery_data.js

---
+++ gallery_data.js
@@ -0,0 +1,18 @@
+
+//Title
+let galleryTitle = "Butterflies";
+
+//list of image files
+let imageFiles = ["AdonisBlue.jpg", "BrownArgus.jpg", "BrownHairstreak.jpg", "ChalkHillBlue.jpg",
+    "ChequeredSkipper.jpg", "Gatekeeper.jpg", "GrizzledSkipper.jpg", "HollyBlue.jpg",
+    "Monarch.jpg", "Peacock.jpg", "RedAdmiral.jpg", "Swallowtail.jpg"]
+
+//captions
+let imageCaptions = ["Adonis Blue", "Brown Argus", "Brown Hairstreak", "Chalk Hill Blue", 
+    "Chequered Skipper", "Gatekeeper", "Grizzled Skipper", "Holly Blue", 
+    "Monarch", "Peacock", "Red Admiral", "Swallowtail"]
+
+//number of image files
+let imageCount = imageFiles.length;
+
+let imageFavourites = [];
